49b5c429b406d0ed162b9d6f863175f136edf67a
Cait <cait@CfE-MacBook-Pro.local>
2017-09-29 21:19:13 -0400

    syntax practice initial commit

---
+++ RPR-Syntax_basics.R
@@ -0,0 +1,16 @@
+# RPR-Syntax basics
+#
+# Purpose: Practice the basics of R syntax, writing a script under verision control
+#
+# Note:    This project lives on Github:
+#          https://github.com/harrig12/RPR-Syntax_basics
+#
+# Version: 1.1
+#
+# Date:    2017  09
+# Author:  Boris Steipe (boris.steipe@utoronto.ca)
+#
+# V 1.1    Add fast phi calculation and complex expression selection
+# V 1.0    Golden Spiral example
+# V 0.2    Additions to integrate with tutorial
+# V 0.1    First code
