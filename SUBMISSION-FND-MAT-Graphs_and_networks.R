b95e570fdf7bca415a2a55352e2333c2c8fecdfe
Cait <cait.harrigan@mail.utoronto.ca>
2017-10-27 19:49:16 -0400

    first code

---
+++ SUBMISSION-FND-MAT-Graphs_and_networks.R
@@ -0,0 +1,18 @@
+# SUBMISSION-FND-MAT-Graphs_and_networks.R
+#
+# Purpose:  A Bioinformatics Course:
+#              R code accompanying the FND-MAT-Graphs_and_networks unit.
+#
+# Version:  1.0
+#
+# Date:     2017  10  27
+# Author:   Cait Harrigan (cait.harrigan@mail.utoronto.ca)
+#
+# Versions:
+#           1.0    First code
+# ==============================================================================
+
+#load data prepared from STRING database
+#load("./data/scCCnet.RData")
+
+#[END]
